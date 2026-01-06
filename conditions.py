e0ac7442eeebb7292ab2aeefc7f18291ade9bcf6
Abdunabi13 <youngpro13022003@gmail.com>
2026-01-06 22:51:13 +0500

    Add example for if-elif-else conditions

---
+++ conditions.py
@@ -0,0 +1,18 @@
+#conditions.py
+
+age=20
+
+if age >=18:
+    print("You are an adult.")
+else:
+    print("You are under 18.")
+
+
+score=85
+
+if score>=90:
+    print("Grade: A")
+elif score >=75:
+    print("Grade: B")
+else:
+    print("Grade: C")
