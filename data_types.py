cda2afdbf8846b0a366e6cf34c68ebe0c4675e48
Abdunabi13 <youngpro13022003@gmail.com>
2026-01-10 00:16:13 +0500

    Add Python data types examples

---
+++ data_types.py
@@ -0,0 +1,69 @@
+#data_types.py
+
+# Get data type
+
+
+# Text Type:    str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+
+# Example:
+
+#Text type
+x="Hello world"
+print(x)
+print(type(x))
+
+# int
+x=20
+print(x)
+print(type(x))
+
+#  float
+x=20.5
+print(x)
+print(type(x))
+
+# complex
+x=1j
+print(x)
+print(type(x))
+
+# list
+x=["apple","banana","cherry"]
+print(x)
+print(type(x))
+
+# tuple
+x=("apple","banana","cherry")
+print(x)
+print(type(x))
+
+# range
+x=range(6)
+print(x)
+print(type(x))
+
+# dict
+x={"name":"John","age":36}
+print(x)
+print(type(x))
+
+# set 
+x={"apple","banana","cherry"}
+print(x)
+print(type(x))
+
+# bool
+x=True
+print(x)
+print(type(x))
+
+x=None
+print(x)
+print(type(x))
