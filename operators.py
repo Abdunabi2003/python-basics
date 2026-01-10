5bee5329d307c607d6fad88245279eef29dc8f16
Abdunabi13 <youngpro13022003@gmail.com>
2026-01-10 21:09:06 +0500

    Add Python operators examples

---
+++ operators.py
@@ -0,0 +1,27 @@
+# operators.py
+# Python Operators Example
+
+# Arithmetic Operators
+print("Arithmetic Operators: ")
+print("If we add 10 to 3, the result is:", 10 + 3)
+print("If we subtract 3 from 10, the result is:", 10 - 3)
+print("If we multiply 10 by 3, the result is:", 10 * 3)
+print("If we divide 10 by 3, the result is:", 10 / 3)
+print("If we divide 10 by 3 and take the remainder, the result is:", 10 % 3)
+print("If we raise 10 to the power of 3, the result is:", 10 ** 3)
+
+
+# Comparison Operators
+
+print("If we check whether 10 is equal to 3, the result is:", 10 == 3)
+print("If we check whether 10 is not equal to 3, the result is:", 10 != 3)
+print("If we check whether 10 is greater than 3, the result is:", 10 > 3)
+print("If we check whether 10 is less than 3, the result is:", 10 < 3)
+print("If we check whether 10 is greater than or equal to 3, the result is:", 10 >= 3)
+print("If we check whether 10 is less than or equal to 3, the result is:", 10 <= 3)
+
+# Logical Operators
+
+print("If we use AND with True and False, the result is:", True and False)
+print("If we use OR with True and False, the result is:", True or False)
+print("If we negate True using NOT, the result is:", not True)
